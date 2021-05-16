b5b1c471b4f79de671aed46af6a59b6962882755
Rohit Veer <rohitveer3004@gmail.com>
2021-05-16 17:17:30 +0530

    inital setup with some mthods

---
+++ lib/parking_lot.rb
@@ -0,0 +1,18 @@
+require_relative './spots'
+
+class ParkingLot
+
+  attr_accessor :spots
+
+  def initialize
+  end
+
+  def park
+    #to park the car
+  end
+
+  def leave
+     #to free the parking slot
+  end
+
+end
---
+++ lib/spots.rb
@@ -0,0 +1,16 @@
+require_relative './car'
+
+class Spot
+
+  def initialize (id)
+  end
+
+  def park
+  end
+
+  def reg_no
+  end
+
+  def car_color
+  end
+end
